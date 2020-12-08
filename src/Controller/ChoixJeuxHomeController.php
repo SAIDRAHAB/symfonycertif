09045a5e8962be5c6d864ed8d6e53c233226cbd7
Your Name <you@example.com>
2020-12-08 20:44:25 +0100

    navbar

---
+++ src/Controller/ChoixJeuxHomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ChoixJeuxHomeController extends AbstractController
+{
+    /**
+     * @Route("/choix/jeux/home", name="choix_jeux_home")
+     */
+    public function index(): Response
+    {
+        return $this->render('choix_jeux_home/index.html.twig', [
+            'controller_name' => 'ChoixJeuxHomeController',
+        ]);
+    }
+}

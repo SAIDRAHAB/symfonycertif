19f7b7a86a2169240223beb21adac6777c7d7633
Your Name <you@example.com>
2020-12-15 05:28:14 +0100

    mail contact ok mailhog

---
+++ assets/app.js
@@ -0,0 +1,18 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+//import './public/css/jeux/hanoi.scss';
+//import './public/css/jeux/memory.css';
+//import './public/css/jeux/smiley.scss';
+
+// start the Stimulus application
+import './bootstrap';
+import './node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "custom";
+import "~bootstrap/scss/bootstrap";
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,30 +4,58 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(): Response
+    public function index(Request $request,MailerInterface $mailer)
     {
+
+         
+
         $formulaire_contact = $this->createFormBuilder()
-            ->add('mail', EmailType::class)
-            ->add('name', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('sujet', TextType::class)
             ->add('message', TextareaType::class)
             ->add('send', SubmitType::class)
             ->getForm();
 
+            $contact = $formulaire_contact->handleRequest($request);
+
+            if($formulaire_contact->isSubmitted() && $formulaire_contact->isValid()){
+                $email = (new TemplatedEmail())
+                    ->from($contact->get('email')->getData())
+                    ->to('saidrahab@hotmail.fr')
+                    ->subject('Contact depuis le site PetitesAnnonces')
+                    ->htmlTemplate('contact/contact.html.twig')
+                    ->context([
+                        'mail' => $contact->get('email')->getData(),
+                        'sujet' => $contact->get('sujet')->getData(),
+                        'message' => $contact->get('message')->getData(),
+                    ])
+                ;
+                
+                $mailer->send($email);
+                
+                $this->addFlash('message', 'Mail de contact envoyé');
+                return $this->redirectToRoute('monprofil');
+            }
+    
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            
             'contact_formulaire' => $formulaire_contact->createView(),
         ]);
     }
